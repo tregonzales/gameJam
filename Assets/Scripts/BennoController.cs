9bb600628162afae9e03b5dda7746618d9e1c71a
Tre Gonzales <Tre@education.com>
2018-02-08 16:06:06 +0100

    does merging work? or am i breaking everything

---
+++ Assets/Scripts/BennoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BennoController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;
+	}
+}
